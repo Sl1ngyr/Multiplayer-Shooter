1f79c0f6f69b756b45ac5b87699288d7391ffe1e
Sl1ngyr <60054609+Sl1ngyr@users.noreply.github.com>
2024-04-11 21:37:52 +0300

    implemented SO WaveData

---
+++ Assets/Scripts/Wave/WaveData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Enemy;
+using Items;
+using UnityEngine;
+
+namespace Wave
+{
+    [CreateAssetMenu(fileName = "WaveData", menuName = "ScriptableObjects/WaveData")]
+    public class WaveData : ScriptableObject
+    {
+        [SerializeField] private float _break;
+        [SerializeField] private float _duration;
+        [SerializeField] private List<BaseEnemyController> _enemies;
+        [SerializeField] private List<BaseItem> _items;
+        
+        public float Break => _break;
+        public float Duration => _duration;
+        public List<BaseEnemyController> Enemies => _enemies;
+        public List<BaseItem> Items => _items;
+    }
+}
